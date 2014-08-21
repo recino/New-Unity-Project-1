8bca7be08e4e39cc5cacaa7bdbadad6fd23d2209
benwurth <ben.wurth@gmail.com>
2014-08-20 18:55:58 -0700

    Goblin AI now moves towards target

---
+++ Assets/Scripts/enemy_AI.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemy_AI : MonoBehaviour {
+
+	//Public variables
+	public GameObject _Target;
+	public float _Speed = 10.0f;
+
+	//Private variables
+	enum EnemyStateEnum {
+		Idle,
+		SeesPlayer,
+		InRange
+	};
+
+//	EnemyStateEnum _EnemyState = EnemyStateEnum.SeesPlayer;
+	EnemyStateEnum _EnemyState;
+
+	// Use this for initialization
+	void Start () {
+
+		_EnemyState = EnemyStateEnum.SeesPlayer;	
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+		//Finite State Machine
+		switch (_EnemyState) 
+		{
+		case EnemyStateEnum.Idle:
+			break;
+		case EnemyStateEnum.SeesPlayer:
+//			Debug.Log("Sees the Player");
+			MoveToPlayer();
+			break;
+		}
+	}
+
+	void MoveToPlayer() {
+		Vector3 playerPosition = _Target.transform.position;
+		Vector3 direction = (playerPosition - transform.position).normalized;
+
+		Debug.Log(playerPosition);
+		Debug.DrawRay(transform.position, direction);
+
+		//Move enemy to player
+//		rigidbody.MovePosition(transform.position + direction * _Speed * Time.deltaTime);
+		transform.position = transform.position + direction * _Speed * Time.deltaTime;
+	}
+}
