ab24f645d09cf3f3147d426e1ebd38de12e7b59f
benwurth <ben.wurth@gmail.com>
2014-08-25 01:08:08 -0700

    Goblin AI updated

--- Assets/Scripts/enemy_AI_characterController.cs
+++ Assets/Scripts/enemy_AI_characterController.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class enemy_AI : MonoBehaviour {
+public class enemy_AI_characterController : MonoBehaviour {
 
 	//Public variables
 	public GameObject _Target;
 	public float _Speed = 10.0f;
 	public float _RotationDamping = 1.0f;
+	public float _Gravity = 20.0f;
 
 	//Private variables
 	enum EnemyStateEnum {
@@ -17,11 +18,14 @@ public class enemy_AI : MonoBehaviour {
 
 	EnemyStateEnum _EnemyState;
 
+	CharacterController controller;
+
 	// Use this for initialization
 	void Start () {
 
 		_EnemyState = EnemyStateEnum.SeesPlayer;	
-	
+		controller = GetComponent<CharacterController>();
+
 	}
 	
 	// Update is called once per frame
@@ -44,17 +48,27 @@ public class enemy_AI : MonoBehaviour {
 	}
 
 	void OnTriggerEnter (Collider other) {
+		Debug.Log(other.gameObject == _Target);
+
 		if (other.gameObject == _Target) {
 			_EnemyState = EnemyStateEnum.InRange;
 		}
 	}
 
+//	void OnCollisionEnter(Collision collision) {
+//		foreach (ContactPoint contact in collision.contacts) {
+//			Debug.DrawRay(contact.point, contact.normal, Color.red);
+//		}
+//	}
+
 	void MoveToPlayer() {
 		Vector3 playerPosition = _Target.transform.position;
+//		playerPosition.y = transform.position.y;
 		Vector3 direction = (playerPosition - transform.position).normalized;
 
 		//Move enemy to player
-		transform.position = transform.position + direction * _Speed * Time.deltaTime;
+//		transform.position = transform.position + direction * _Speed * Time.deltaTime;
+		controller.SimpleMove(direction * _Speed * Time.deltaTime);
 	}
 
 	void RotateToPlayer() {
@@ -62,6 +76,7 @@ public class enemy_AI : MonoBehaviour {
 		Vector3 direction = (playerPosition - transform.position).normalized;
 
 		Quaternion rotation = Quaternion.LookRotation(direction);
+		rotation = Quaternion.Euler(new Vector3(0f, rotation.eulerAngles.y, 0f));
 		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * _RotationDamping);
 	}
 }
