bdc4ab1b3b1b64be2eaa5f20e40af8c8d5e43aeb
benwurth <ben.wurth@gmail.com>
2014-08-25 02:52:35 -0700

    Goblin now starts and stops properly

--- Assets/Scripts/enemy_AI_characterController.cs
+++ Assets/Scripts/enemy_AI_characterController.cs
@@ -5,6 +5,8 @@ public class enemy_AI_characterController : MonoBehaviour {
 
 	//Public variables
 	public GameObject _Target;
+	public float _viewDistance = 10.0f;
+	public float _attackDistance = 5.0f;
 	public float _Speed = 10.0f;
 	public float _RotationDamping = 1.0f;
 	public float _Gravity = 20.0f;
@@ -30,8 +32,10 @@ public class enemy_AI_characterController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
-
+		CheckTargetPosition(_Target);
+		Debug.Log(DetectDistance(_Target));
+		Debug.Log(_EnemyState);
+		
 		//Finite State Machine
 		switch (_EnemyState) 
 		{
@@ -42,24 +46,11 @@ public class enemy_AI_characterController : MonoBehaviour {
 			MoveToPlayer();
 			break;
 		case EnemyStateEnum.InRange:
+//			Debug.Log("Attack!");
 			RotateToPlayer();
 			break;
 		}
 	}
-
-	void OnTriggerEnter (Collider other) {
-		Debug.Log(other.gameObject == _Target);
-
-		if (other.gameObject == _Target) {
-			_EnemyState = EnemyStateEnum.InRange;
-		}
-	}
-
-//	void OnCollisionEnter(Collision collision) {
-//		foreach (ContactPoint contact in collision.contacts) {
-//			Debug.DrawRay(contact.point, contact.normal, Color.red);
-//		}
-//	}
 
 	void MoveToPlayer() {
 		Vector3 playerPosition = _Target.transform.position;
@@ -67,7 +58,6 @@ public class enemy_AI_characterController : MonoBehaviour {
 		Vector3 direction = (playerPosition - transform.position).normalized;
 
 		//Move enemy to player
-//		transform.position = transform.position + direction * _Speed * Time.deltaTime;
 		controller.SimpleMove(direction * _Speed * Time.deltaTime);
 	}
 
@@ -78,5 +68,25 @@ public class enemy_AI_characterController : MonoBehaviour {
 		Quaternion rotation = Quaternion.LookRotation(direction);
 		rotation = Quaternion.Euler(new Vector3(0f, rotation.eulerAngles.y, 0f));
 		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * _RotationDamping);
+	}
+
+	float DetectDistance (GameObject target) {
+		return Vector3.Distance(transform.position, target.transform.position);
+	}
+
+	void CheckTargetPosition (GameObject target) {
+//		if (_attackDistance > DetectDistance(target)) {
+//			_EnemyState = EnemyStateEnum.InRange;
+//		}
+
+		if (DetectDistance(target) > _viewDistance) {
+			_EnemyState = EnemyStateEnum.Idle;
+		}
+		else if (_viewDistance > DetectDistance(target) && DetectDistance(target) > _attackDistance) {
+			_EnemyState = EnemyStateEnum.SeesPlayer;
+		}
+		else if (_attackDistance > DetectDistance(target)){
+			_EnemyState = EnemyStateEnum.InRange;
+		}
 	}
 }
